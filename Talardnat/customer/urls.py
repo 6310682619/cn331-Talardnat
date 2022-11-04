6367f937770f421dda81b33725ca85bccdc3f11e
Kemika Weerakulwatana <6310682619@cn330>
2022-11-04 20:50:14 +0700

    register form

--- Talardnat/customer/urls.py
+++ Talardnat/customer/urls.py
@@ -3,9 +3,10 @@ from . import views
 from talard import views as talard_view
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('about', views.about, name='about'),
-    path('login', views.login_view, name='login'),
-    path('logout', views.logout_view, name='logout'),
-    path('register', views.register, name='register'),
+    path('', talard_view.index, name='homepage'),
+    path('about', talard_view.about, name='about'),
+    path('customer/login', views.login_view, name='login'),
+    path('customer/logout', views.logout_view, name='logout'),
+    path('customer/register', views.register, name='register'),
+    path('customer/<int:u_id>/profile/', views.profile, name='profile'),
 ]
--- Talardnat/talard/urls.py
+++ Talardnat/talard/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('about', views.about, name='about'),
+    path('talard/', views.index, name='talard'),
+    path('talard/shop', views.index, name='allshop'),
+    path('talard/shop/<int:shop_id>/', views.index, name='shop'),
 ]
