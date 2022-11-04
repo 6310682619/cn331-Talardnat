1b4463ae06d903246db91ca02b58be1e98ada3ae
Chawanrat Gaysornkaew <6310611014@cn102>
2022-11-04 22:29:10 +0700

    signup

--- Talardnat/seller/urls.py
+++ Talardnat/seller/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name='seller_index'),
     path('login', views.login_view, name='seller_login'),
     path('logout', views.logout_view, name='seller_logout'),
+    path('signup', views.signup, name='seller_signup'),
     ]
