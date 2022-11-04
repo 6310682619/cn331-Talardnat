d019158221c78e1aa79330df11ca52ce91eb6eba
Runchana Supasavate <6310682650@cn331.com>
2022-11-05 02:28:55 +0700

    add url, edit html, css

--- Talardnat/seller/urls.py
+++ Talardnat/seller/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 from . import views
+from talard import views as tview
 
 urlpatterns = [
+    path("talard", tview.index, name="taview"),
     path('', views.index, name='seller_index'),
     path('login', views.login_view, name='seller_login'),
     path('logout', views.logout_view, name='seller_logout'),
