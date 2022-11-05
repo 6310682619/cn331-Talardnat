95d9ebd068b31510b9b60ca409c39e4158c77470
Kemika Weerakulwatana <6310682619@cn330>
2022-11-05 19:45:00 +0700

    customer register v.1

--- Talardnat/customer/admin.py
+++ Talardnat/customer/admin.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
-from .models import customer_profile
+from .models import Profile
 
 # Register your models here.
 
-class SignUpAdmin(admin.ModelAdmin):
-     pass
-
-admin.site.register(customer_profile)
+admin.site.register(Profile)
