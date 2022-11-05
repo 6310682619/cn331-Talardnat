f9e20ce4095a3ab6682f28b8123ebfccaf0b95e1
Chawanrat Gaysornkaew <6310611014@cn102>
2022-11-05 23:44:10 +0700

    create delete shop

--- Talardnat/myshop/views.py
+++ Talardnat/myshop/views.py
@@ -5,18 +5,35 @@ from seller.models import seller_detail
 from django.urls import reverse, exceptions
 from django.http import HttpResponseRedirect
 from django.contrib.auth.models import User
-from .forms import ProductForm
+from .forms import ProductForm, ShopForm
 # Create your views here.
 
 def index(request, sid):
     shop = shop_detail.objects.filter(seller_id= sid)
-    return render(request, "myshop/myshop_index.html", {"shop":shop})
+    seller = seller_detail.objects.get(pk = sid)
+    if request.method == 'POST':
+        form = ShopForm(request.POST)
+        if form.is_valid():
+            newshop = form.save(commit=False)
+            newshop.seller_id = seller
+            newshop.save()
+    else:
+        form = ShopForm()
+    return render(request, "myshop/myshop_index.html", {'form' : form, "shop":shop})
 
 def shop(request, shop_id):
     s = shop_detail.objects.get(pk=shop_id)
     user = User.objects.get(username=s.seller_id)
     seller = seller_detail.objects.get(sname = user)
     return render(request, "myshop/shop.html", {"shop" : s, "seller":seller.id})
+
+def del_shop(request, shop_id):
+    s = shop_detail.objects.get(pk = shop_id)
+    user = User.objects.get(username=s.seller_id)
+    seller = seller_detail.objects.get(sname = user)
+    sid = seller.id
+    s.delete()
+    return HttpResponseRedirect(reverse("myshop_index", args=(sid,)))
 
 def product(request,shop_id):
     s = shop_detail.objects.get(pk=shop_id)
