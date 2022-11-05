eba8d83d09a3981585378aaceb500f41a6ba99f0
Kemika Weerakulwatana <6310682619@cn330>
2022-11-05 22:14:27 +0700

    create talard

--- Talardnat/talard/urls.py
+++ Talardnat/talard/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('about', views.about, name='about'),
-    path('talard/', views.index, name='talard'),
-    path('talard/shop', views.index, name='allshop'),
+    path('talard/', views.category, name='talard'),
+    path('talard/shop/<str:category>', views.allshop, name='allshop'),
     path('talard/shop/<int:shop_id>/', views.index, name='shop'),
 ]
