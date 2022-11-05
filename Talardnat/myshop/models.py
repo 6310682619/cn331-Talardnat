00001f6ab6be49a325815b6e37294d42beeb587d
Chawanrat Gaysornkaew <6310611014@cn102>
2022-11-05 19:21:31 +0700

    product must pip install Pillow

--- Talardnat/myshop/models.py
+++ Talardnat/myshop/models.py
@@ -8,9 +8,19 @@ class shop_detail(models.Model):
     seller_id = models.ForeignKey(seller_detail, on_delete=models.CASCADE, related_name = "shop", null=True)
     name = models.CharField(max_length=64)
     category = models.CharField(max_length=64, null=True)
-    interact = models.CharField(max_length=4, null=True)
+    interact = models.CharField(max_length=200, null=True)
     expire = models.IntegerField(default=0)
     queue = models.IntegerField(null=True)
 
     def __str__(self):
         return f"{self.id} name: {self.name} queue: {self.queue}"
+
+class product(models.Model):
+    shop = models.ForeignKey(shop_detail, on_delete=models.CASCADE, related_name = "prod", null=True)
+    product_name = models.CharField(max_length=64)
+    price = models.IntegerField(null=True)
+    product_im = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return f"{self.id} product: {self.product_name}"
+
--- Talardnat/myshop/urls.py
+++ Talardnat/myshop/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path, include
 from . import views
 
+
 urlpatterns = [
     path('<int:sid>/', views.index, name='myshop_index'),
     path('shop/<int:shop_id>/', views.shop, name='myshop'),
+    path('shop/<int:shop_id>/product/', views.product, name='product'),
+    path('shop/<int:shop_id>/<int:prod_id>/delprod/', views.del_prod, name='delprod'),
     ]
