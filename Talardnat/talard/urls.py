c08b24422aac533f07cebc5f2067423a6b060124
Chawanrat Gaysornkaew <6310611014@cn102>
2022-11-12 17:23:10 +0700

    try

--- Talardnat/talard/urls.py
+++ Talardnat/talard/urls.py
@@ -7,14 +7,10 @@ urlpatterns = [
     path('talard/<int:u_id>/', views.category, name='talard'),
     path('talard/<str:category>/<int:u_id>/', views.allshop, name='allshop'),
     path('talard/shop/<int:u_id>/<int:shop_id>/', views.thisshop, name='thisshop'),
-<<<<<<< HEAD
     path('talard/shop/<int:u_id>/<int:shop_id>/<int:prod_id>/buy/', views.buy, name='buy'),
     path('talard/<int:u_id>/order/', views.order, name='order'),
     path('talard/<int:u_id>/<int:oid>/order/', views.del_order, name='delorder'),
-=======
-    path('talard/shop/<int:u_id>/<int:shop_id>/<int:prod_id>/buy', views.buy, name='buy'),
     path('talard/shop/<int:u_id>/<int:shop_id>/review', views.addreview, name='addreview'),
     path('rateus/', views.rate, name='rateus'),
     #path('talard/shop/<int:u_id>/<int:shop_id>/review/addreview', views.addreview, name='addreview'),
->>>>>>> c19ac2b4704b779faeb114d650f48c0fa3f21c09
 ]
