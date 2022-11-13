8bd4351f4ca921a92889c94d853be7de7dbe6708
Kemika Weerakulwatana <6310682619@student.tu.ac.th>
2022-11-13 13:48:26 +0700

    testing

--- Talardnat/talard/test_views.py
+++ Talardnat/talard/test_views.py
@@ -1,6 +1,7 @@
 from django.test import TestCase, Client
 from django.urls import reverse
 from django.contrib.auth.models import User
+from .models import *
 from customer.models import Profile
 from myshop.models import shop_detail, product, review
 from seller.models import seller_detail
@@ -23,11 +24,16 @@ class TalardViewsTest(TestCase):
         user2 = User.objects.create_user(username='monday', password='monday22', email='monday@morning.com')
 
         seller1 = seller_detail.objects.create(
-            sname = user2
+            sname = user1
         )
 
         customer1 = Profile.objects.create(
-            customer = user2
+            customer = user2,
+            address = "Citypark",
+            city = "TU",
+            state = "Bkk",
+            zip = 11111,
+            phone = 123456789
         )
 
         shop1 = shop_detail.objects.create(
@@ -36,8 +42,6 @@ class TalardViewsTest(TestCase):
             category = "food",
             in_interact = "Chocolate is the best",
             ex_interact = "Your favourite Choco!",
-            expire = 14,
-            queue = 1
         )
 
         temp_img = tempfile.NamedTemporaryFile()
@@ -51,27 +55,38 @@ class TalardViewsTest(TestCase):
             count = 10
         )
 
+        review = Review.objects.create(
+            user = customer1,
+            shop = shop1,
+            review_text = "so good",
+            review_rating = 5
+        )
+
     def test_index_view(self):
+        """Test if page is accessible, check response and template"""
+        
         c = Client()
         response=c.get(reverse('index'))
-        # Check response
+
         self.assertEqual(response.status_code, 200)
-        # Check template
         self.assertTemplateUsed(response, 'talard/index.html')
 
     def test_category_view(self):
+        """Test if page is accessible, check response and template"""
+
         c = Client()
         c.post(reverse('customer_login'),
                {'username': 'sunday', 
                'password': 'sunday11'})
         customer1 = Profile.objects.first()
         response=c.get(reverse('talard',args=[customer1.id]))
-        # Check response
+
         self.assertEqual(response.status_code, 200)
-        # Check template
         self.assertTemplateUsed(response, 'talard/category.html')
 
     def test_allshop_view(self):
+        """Test if page is accessible, check response and template"""
+
         c = Client()
         c.post(reverse('customer_login'),
                {'username': 'sunday', 
@@ -80,12 +95,12 @@ class TalardViewsTest(TestCase):
         customer1 = Profile.objects.first()
                
         response=c.get(reverse('allshop', args=[shop1.category, customer1.id]))
-        # Check response
         self.assertEqual(response.status_code, 200)
-        # Check template
         self.assertTemplateUsed(response, 'talard/allshop.html')
 
     def test_thisshop_view(self):
+        """Test if page is accessible, check response and template"""
+
         c = Client()
         c.post(reverse('customer_login'),
                {'username': 'sunday', 
@@ -93,19 +108,40 @@ class TalardViewsTest(TestCase):
         shop1 = shop_detail.objects.first()
         customer1 = Profile.objects.first()
 
-        response=c.get(reverse('thisshop', args=[customer1.id,shop1.id]))
-        # Check response
+        response=c.get(reverse('thisshop', args=[customer1.id, shop1.id]))
         self.assertEqual(response.status_code, 200)
-        # Check template
         self.assertTemplateUsed(response, 'talard/shop.html')
 
     def test_about_view(self):
+        """Test if about page is accessible, check response and template"""
+
         c = Client()
         response=c.get(reverse('about'))
-        # Check response
         self.assertEqual(response.status_code, 200)
-        # Check template
         self.assertTemplateUsed(response, 'talard/about.html')
+
+    def test_not_user_rateus(self):
+        """Test if unauthorized user rate website return login page"""
+        
+        c = Client()
+        c.post(reverse('rating'),
+            {'rate_text' : 'good',
+                'rating' : 5})
+        response=c.get(reverse('customer_login'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_user_rateus(self):
+        """Test if authorized user rate website"""
+        
+        c = Client()
+        c.post(reverse('customer_login'),
+               {'username': 'sunday', 
+               'password': 'sunday11'})
+        response=c.get(reverse('rating'))
+        self.assertEqual(response.status_code, 200)
+        response=c.get(reverse('rateus'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'talard/rate.html')
 
     # def test_buy_view(self):
     #     c = Client()
